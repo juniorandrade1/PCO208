aca3d60626170fa37d18bee5702a76142598732f
juniorandrade1 <junior.andrade.cco@gmail.com>
2018-09-30 15:18:57 -0300

    Robot

---
+++ robot_movimentation.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Action {
+  Action(){};
+};
+
+struct State {
+  State(){};
+};
+
+int main() {
+  int n, m; scanf("%d %d", &n, &m);
+  vector< vector< State > > matrix(n, vector< State >(m));
+  return 0;
+}
